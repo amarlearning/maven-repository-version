f649d2f86decf91c3938e3d4701aa6698de8389e
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-02-19 03:24:40 +0530

    Removed old files

--- README.md
+++
@@ -1,2 +0,0 @@
-# maven-repository-version
-Find the latest verison of any maven repository using group-id and artifact-id
--- src/com/amarpandey/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.amarpandey;
-
-public class Constants {
-
-	public static final String BASE_URL = "https://mvnrepository.com/artifact/";
-	
-}
