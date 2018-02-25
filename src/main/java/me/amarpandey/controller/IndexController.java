035eb3cad394da447a9cd4a31b665b5d20349d3a
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-02-25 17:17:32 +0530

    removed spring-devtool dep and added angular 1.6

--- src/main/java/me/amarpandey/controller/IndexController.java
+++ src/main/java/me/amarpandey/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	@RequestMapping(value = "/home", method = RequestMethod.POST)
 	public String index(@RequestBody String pomxml) {
 		return pomxml;
 	}
