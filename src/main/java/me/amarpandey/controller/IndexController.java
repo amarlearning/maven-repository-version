e9c01e1eb07252dbc4c80614bfb19eece8017570
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-02-26 23:43:10 +0530

    removed angular and form created

--- src/main/java/me/amarpandey/controller/IndexController.java
+++ src/main/java/me/amarpandey/controller/IndexController.java
@@ -1,15 +1,15 @@
 package me.amarpandey.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-	@RequestMapping(value = "/home", method = RequestMethod.POST)
-	public String index(@RequestBody String pomxml) {
-		return pomxml;
+	@PostMapping(value = "/submitXML", consumes = "application/x-www-form-urlencoded")
+	public String submitXML(@RequestParam("xmlData") String xmlData) {
+		return xmlData;
 	}
 }
--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,15 +1,15 @@
 (function() {
-	
+
 	'use strict';
-	
+
 	var editor = ace.edit("editor");
-	
+
 	editor.setShowPrintMargin(false);
 	editor.setTheme("ace/theme/monokai");
 	editor.session.setMode("ace/mode/xml");
 	
-	$("#generatepom").click(function() {
-		console.log("Ace editor value : " + editor.getValue());
+	$("#submitXML").submit(function() {
+		$("input[name=xmlData]").val(editor.getValue());
 	});
-	
+
 })();
