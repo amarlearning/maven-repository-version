3d1ed182579d91cf4b190ad5d3f5fd18ed7ed25d
Amar Prakash Pandey <amar.om1994@gmail.com>
2018-02-28 23:57:52 +0530

    Submitting form via Jquery

--- src/main/java/me/amarpandey/controller/IndexController.java
+++ src/main/java/me/amarpandey/controller/IndexController.java
@@ -1,6 +1,5 @@
 package me.amarpandey.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -7,9 +7,23 @@
 	editor.setShowPrintMargin(false);
 	editor.setTheme("ace/theme/monokai");
 	editor.session.setMode("ace/mode/xml");
-	
+
+	/*$("#submitXML").submit(function() {
+
+		var xmlText = new XMLSerializer().serializeToString(editor.getValue());
+
+		$("input[name=xmlData]").val(xmlText);
+		alert($("input[name=xmlData]").val());
+		console.log($("input[name=xmlData]").val());
+	});*/
+
 	$("#submitXML").submit(function() {
-		$("input[name=xmlData]").val(editor.getValue());
+		alert("new y");
+		$.post("/submitXML", {
+			'data' : editor.getValue()
+		}, function(data) {
+			console.log("Data is : " + data);
+		});
 	});
 
 })();
